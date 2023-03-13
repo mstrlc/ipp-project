6dcf7891986c8c315c6a40e9c5a44d4eae9bff18
Matyáš Strelec <matyasstrelec@gmail.com>
2023-03-13 18:34:07 +0100

    reorganize code with classes

--- parse.php
+++ parse.php
@@ -4,181 +4,6 @@
 // Project 1 - Parser of IPPcode23
 // Author: Matyas Strelec (xstrel03)
 
-// Global variables for storing regexes used in the script
-$var_regex = '/^(GF|TF|LF)@([a-zA-Z]|_|-|\$|&|%|\*|!|\?)([a-zA-Z0-9]|_|-|\$|&|%|\*|!|\?)*$/';
-$label_regex = '/^([a-zA-Z]|_|-|\$|&|%|\*|!|\?)([a-zA-Z0-9]|_|-|\$|&|%|\*|!|\?)*$/';
-
-/**
- * If the number of tokens is not equal to the expected number, exit with error code 23
- * 
- * @param $number - actual number of tokens
- * @param $expected - expected number of tokens
- */
-function check_number_of_tokens($number, $expected)
-{
-    if ($number != $expected) {
-        fwrite(STDERR, "Error: Wrong number of arguments! Expected $expected, got $number.\n");
-        exit(23);
-    }
-}
-
-/**
- * Check if the variable given is syntactically correct
- * Else, exit with error code 23
- * 
- * @param $token - token to be checked
- */
-function check_var($token)
-{
-    if (!preg_match($GLOBALS['var_regex'], $token)) {
-        fwrite(STDERR, "Error: Invalid variable $token!\n");
-        exit(23);
-    }
-}
-
-/**
- * Check if the label given is syntactically correct
- * Else, exit with error code 23
- * 
- * @param $token - token to be checked
- */
-
-function check_label($token)
-{
-    if (!preg_match($GLOBALS['label_regex'], $token)) {
-        fwrite(STDERR, "Error: Invalid label $token!\n");
-        exit(23);
-    }
-}
-
-/**
- * Check if the symbol given is syntactically correct
- * Else, exit with error code 23
- * 
- * @param $token - token to be checked
- */
-
-function check_symb($token)
-{
-    // Separate the type and value of the symbol
-    $type = explode("@", $token)[0];
-    $val = explode("@", $token)[1];
-
-    // First, check if the symbol is not a variable
-    // If it is, return as it is syntactically correct
-    // If it is not, run more checks for syntax correctness
-    if (!preg_match($GLOBALS['var_regex'], $token)) {
-
-        // Check for integers
-        if ($type == "int") {
-            // Integer can either be simple number, hexademical or octal number, and it cannot be empty
-            if (is_numeric($val) || preg_match('/^0[xX][0-9a-fA-F]+(_[0-9a-fA-F]+)*$/', $val) || preg_match('/^0[oO]?[0-7]+(_[0-7]+)*$/', $val) || !empty($val)) {
-                return;
-            } else {
-                fwrite(STDERR, "Error: Invalid symbol $token!\n");
-                exit(23);
-            }
-
-            // Check for booleans
-        } else if ($type == "bool" && ($val == "true" || $val == "false")) {
-            return;
-        }
-
-        // Check for strings
-        else if ($type == "string") {
-            // Escape sequences are allowed, but they must be in the correct format
-            if (str_contains($val, "\\")) {
-                // Count the number of backslashes - '\\' because of escaping in PHP
-                $num_of_backslashes = substr_count($val, "\\");
-                // Count the number of escape sequences as per the regex
-                $regex_matches = 0;
-                preg_match_all("/\\\\[0-9]{3}/", $val, $regex_matches);
-                // If the number of backslashes is not equal to the number of escape sequences, there is an invalid escape sequence
-                // In this case, exit with error code 23
-                if ($num_of_backslashes != count($regex_matches[0])) {
-                    fwrite(STDERR, "Error: Invalid symbol $token!\n");
-                    exit(23);
-                } else
-                    return;
-            }
-            return;
-
-        // Check for nil type
-        } else if ($type == "nil" && $val == "nil")
-            return;
-
-        // Symbol doesn't match any allowed syntax, exit with error code 23
-        else {
-            fwrite(STDERR, "Error: Invalid symbol $token!\n");
-            exit(23);
-        }
-    }
-    // Symbol is a variable, return correct
-    else {
-        return;
-    }
-}
-
-/**
- * Check if the type given is one of the allowed types
- * Else, exit with error code 23
- * 
- * @param $token - token to be checked
- */
-
-function check_type($token)
-{
-    if ($token != "int" && $token != "bool" && $token != "string" && $token != "nil") {
-        fwrite(STDERR, "Error: Invalid type $token!\n");
-        exit(23);
-    }
-}
-
-/**
- * Write the XML representation of an argument to stdout
- *  
- * @param $xml - object of the xmlwriter class
- * @param $num - number of the token in operation arguments
- * @param $token - token to be written
- */
-function write_arg($xml, $num, $token)
-{
-    // <var> or <const>
-    if (str_contains($token, "@")) {
-        $type = explode("@", $token)[0];
-
-        // <var>
-        if ($type == "GF" || $type == "TF" || $type == "LF") {
-            $type = "var";
-            $value = $token;
-        }
-        // <const>
-        else if ($type == "int" || $type == "bool" || $type == "string" || $type == "nil") {
-            $value = explode("@", $token)[1];
-        }
-    }
-    // <type> or <label>
-    else {
-        // <type>
-        if ($token == "int" || $token == "bool" || $token == "string" || $token == "nil") {
-            $type = "type";
-            $value = $token;
-        }
-        // <label>
-        else {
-            $type = "label";
-            $value = $token;
-        }
-    }
-
-    // Write the argument to stdout
-    xmlwriter_start_element($xml, "arg" . $num);
-    xmlwriter_start_attribute($xml, "type");
-    xmlwriter_text($xml, $type);
-    xmlwriter_end_attribute($xml);
-    xmlwriter_text($xml, $value);
-    xmlwriter_end_element($xml);
-}
 
 /**
  * Print the help message
@@ -227,16 +52,7 @@ if (empty(trim($input))) {
 // Split the input into lines
 $lines = explode("\n", $input);
 
-// Create the XML writer object
-$xml = xmlwriter_open_memory();
-xmlwriter_set_indent($xml, 1);
-xmlwriter_set_indent_string($xml, "\t");
-
-// Write the XML header
-xmlwriter_start_document($xml, '1.0', 'UTF-8');
-xmlwriter_start_element($xml, 'program');
-xmlwriter_start_attribute($xml, 'language');
-xmlwriter_text($xml, 'IPPcode23');
+$xml = Writer::begin_xml();
 
 // Go through each line and clean up the input
 foreach ($lines as $index => &$line) {
@@ -290,25 +106,26 @@ foreach ($lines as $line) {
         case "POPFRAME":
         case "RETURN":
         case "BREAK":
-            check_number_of_tokens($num_of_tokens, 1);
+            Parser::check_number_of_tokens($num_of_tokens, 1);
             break;
+
 
         // 1 argument
         // <var>
         case "DEFVAR":
         case "POPS":
-            check_number_of_tokens($num_of_tokens, 2);
-            check_var($tokens[1]);
-            write_arg($xml, 1, $tokens[1]);
+            Parser::check_number_of_tokens($num_of_tokens, 2);
+            Parser::check_var($tokens[1]);
+            Writer::write_arg($xml, 1, $tokens[1]);
             break;
 
         // <label>
         case "CALL":
         case "LABEL":
         case "JUMP":
-            check_number_of_tokens($num_of_tokens, 2);
-            check_label($tokens[1]);
-            write_arg($xml, 1, $tokens[1]);
+            Parser::check_number_of_tokens($num_of_tokens, 2);
+            Parser::check_label($tokens[1]);
+            Writer::write_arg($xml, 1, $tokens[1]);
             break;
 
         // <symb>
@@ -316,9 +133,9 @@ foreach ($lines as $line) {
         case "WRITE":
         case "EXIT":
         case "DPRINT":
-            check_number_of_tokens($num_of_tokens, 2);
-            check_symb($tokens[1]);
-            write_arg($xml, 1, $tokens[1]);
+            Parser::check_number_of_tokens($num_of_tokens, 2);
+            Parser::check_symb($tokens[1]);
+            Writer::write_arg($xml, 1, $tokens[1]);
             break;
 
         // 2 arguments
@@ -328,20 +145,20 @@ foreach ($lines as $line) {
         case "INT2CHAR":
         case "STRLEN":
         case "TYPE":
-            check_number_of_tokens($num_of_tokens, 3);
-            check_var($tokens[1]);
-            check_symb($tokens[2]);
-            write_arg($xml, 1, $tokens[1]);
-            write_arg($xml, 2, $tokens[2]);
+            Parser::check_number_of_tokens($num_of_tokens, 3);
+            Parser::check_var($tokens[1]);
+            Parser::check_symb($tokens[2]);
+            Writer::write_arg($xml, 1, $tokens[1]);
+            Writer::write_arg($xml, 2, $tokens[2]);
             break;
 
         // <var> <type>    
         case "READ":
-            check_number_of_tokens($num_of_tokens, 3);
-            check_var($tokens[1]);
-            check_type($tokens[2]);
-            write_arg($xml, 1, $tokens[1]);
-            write_arg($xml, 2, $tokens[2]);
+            Parser::check_number_of_tokens($num_of_tokens, 3);
+            Parser::check_var($tokens[1]);
+            Parser::check_type($tokens[2]);
+            Writer::write_arg($xml, 1, $tokens[1]);
+            Writer::write_arg($xml, 2, $tokens[2]);
             break;
 
         // 3 arguments
@@ -359,25 +176,25 @@ foreach ($lines as $line) {
         case "CONCAT":
         case "GETCHAR":
         case "SETCHAR":
-            check_number_of_tokens($num_of_tokens, 4);
-            check_var($tokens[1]);
-            check_symb($tokens[2]);
-            check_symb($tokens[3]);
-            write_arg($xml, 1, $tokens[1]);
-            write_arg($xml, 2, $tokens[2]);
-            write_arg($xml, 3, $tokens[3]);
+            Parser::check_number_of_tokens($num_of_tokens, 4);
+            Parser::check_var($tokens[1]);
+            Parser::check_symb($tokens[2]);
+            Parser::check_symb($tokens[3]);
+            Writer::write_arg($xml, 1, $tokens[1]);
+            Writer::write_arg($xml, 2, $tokens[2]);
+            Writer::write_arg($xml, 3, $tokens[3]);
             break;
 
         // <label> <symb> <symb>
         case "JUMPIFEQ":
         case "JUMPIFNEQ":
-            check_number_of_tokens($num_of_tokens, 4);
-            check_label($tokens[1]);
-            check_symb($tokens[2]);
-            check_symb($tokens[3]);
-            write_arg($xml, 1, $tokens[1]);
-            write_arg($xml, 2, $tokens[2]);
-            write_arg($xml, 3, $tokens[3]);
+            Parser::check_number_of_tokens($num_of_tokens, 4);
+            Parser::check_label($tokens[1]);
+            Parser::check_symb($tokens[2]);
+            Parser::check_symb($tokens[3]);
+            Writer::write_arg($xml, 1, $tokens[1]);
+            Writer::write_arg($xml, 2, $tokens[2]);
+            Writer::write_arg($xml, 3, $tokens[3]);
             break;
 
         // No instruction found
@@ -391,10 +208,238 @@ foreach ($lines as $line) {
     $instruction_order++;
 }
 
-// Finish the XML
-xmlwriter_end_document($xml);
+// End the program
+Writer::finish_xml($xml);
 
-// Write XML to stdout
-echo xmlwriter_output_memory($xml);
+class Parser
+{
+    // Global variables for storing regexes used in the script
+    public static $var_regex = '/^(GF|TF|LF)@([a-zA-Z]|_|-|\$|&|%|\*|!|\?)([a-zA-Z0-9]|_|-|\$|&|%|\*|!|\?)*$/';
+    public static $label_regex = '/^([a-zA-Z]|_|-|\$|&|%|\*|!|\?)([a-zA-Z0-9]|_|-|\$|&|%|\*|!|\?)*$/';
+
+    /**
+     * If the number of tokens is not equal to the expected number, exit with error code 23
+     * 
+     * @param $number - actual number of tokens
+     * @param $expected - expected number of tokens
+     */
+    public static function check_number_of_tokens($number, $expected)
+    {
+        if ($number != $expected) {
+            fwrite(STDERR, "Error: Wrong number of arguments! Expected $expected, got $number.\n");
+            exit(23);
+        }
+    }
+
+    /**
+     * Check if the variable given is syntactically correct
+     * Else, exit with error code 23
+     * 
+     * @param $token - token to be checked
+     */
+    public static function check_var($token)
+    {
+        if (!preg_match(Parser::$var_regex, $token)) {
+            fwrite(STDERR, "Error: Invalid variable $token!\n");
+            exit(23);
+        }
+    }
+
+    /**
+     * Check if the label given is syntactically correct
+     * Else, exit with error code 23
+     * 
+     * @param $token - token to be checked
+     */
+    public static function check_label($token)
+    {
+        if (!preg_match(Parser::$label_regex, $token)) {
+            fwrite(STDERR, "Error: Invalid label $token!\n");
+            exit(23);
+        }
+    }
+
+    /**
+     * Check if the symbol given is syntactically correct
+     * Else, exit with error code 23
+     * 
+     * @param $token - token to be checked
+     */
+    public static function check_symb($token)
+    {
+        // Separate the type and value of the symbol
+        $type = explode("@", $token)[0];
+        $val = explode("@", $token)[1];
+
+        // First, check if the symbol is not a variable
+        // If it is, return as it is syntactically correct
+        // If it is not, run more checks for syntax correctness
+        if (!preg_match(Parser::$var_regex, $token)) {
+
+            // Check for integers
+            if ($type == "int") {
+                // Integer can either be simple number, hexademical or octal number, and it cannot be empty
+                if (is_numeric($val) || preg_match('/^0[xX][0-9a-fA-F]+(_[0-9a-fA-F]+)*$/', $val) || preg_match('/^0[oO]?[0-7]+(_[0-7]+)*$/', $val) || !empty($val)) {
+                    return;
+                } else {
+                    fwrite(STDERR, "Error: Invalid symbol $token!\n");
+                    exit(23);
+                }
+
+                // Check for booleans
+            } else if ($type == "bool" && ($val == "true" || $val == "false")) {
+                return;
+            }
+
+            // Check for strings
+            else if ($type == "string") {
+                // Escape sequences are allowed, but they must be in the correct format
+                if (str_contains($val, "\\")) {
+                    // Count the number of backslashes - '\\' because of escaping in PHP
+                    $num_of_backslashes = substr_count($val, "\\");
+                    // Count the number of escape sequences as per the regex
+                    $regex_matches = 0;
+                    preg_match_all("/\\\\[0-9]{3}/", $val, $regex_matches);
+                    // If the number of backslashes is not equal to the number of escape sequences, there is an invalid escape sequence
+                    // In this case, exit with error code 23
+                    if ($num_of_backslashes != count($regex_matches[0])) {
+                        fwrite(STDERR, "Error: Invalid symbol $token!\n");
+                        exit(23);
+                    } else
+                        return;
+                }
+                return;
+
+                // Check for nil type
+            } else if ($type == "nil" && $val == "nil")
+                return;
+
+            // Symbol doesn't match any allowed syntax, exit with error code 23
+            else {
+                fwrite(STDERR, "Error: Invalid symbol $token!\n");
+                exit(23);
+            }
+        }
+        // Symbol is a variable, return correct
+        else {
+            return;
+        }
+    }
+
+    /**
+     * Check if the type given is one of the allowed types
+     * Else, exit with error code 23
+     * 
+     * @param $token - token to be checked
+     */
+    public static function check_type($token)
+    {
+        if ($token != "int" && $token != "bool" && $token != "string" && $token != "nil") {
+            fwrite(STDERR, "Error: Invalid type $token!\n");
+            exit(23);
+        }
+    }
+}
+
+class Writer
+{
+    /**
+     * Create the XML writer object and set up the indentation
+     * 
+     * @return $xml - object of the xmlwriter class
+     */
+    private static function create_xml_writer()
+    {
+        $xml = xmlwriter_open_memory();
+        xmlwriter_set_indent($xml, 1);
+        xmlwriter_set_indent_string($xml, "\t");
+        return $xml;
+    }
+
+    /**
+     * Write the XML header to stdout
+     * 
+     * @param $xml - object of the xmlwriter class
+     */
+    private static function write_xml_header($xml)
+    {
+        xmlwriter_start_document($xml, '1.0', 'UTF-8');
+        xmlwriter_start_element($xml, 'program');
+        xmlwriter_start_attribute($xml, 'language');
+        xmlwriter_text($xml, 'IPPcode23');
+    }
+
+    /**
+     * Write the XML header to stdout and set up the XML writer object
+     * 
+     * @return $xml - object of the xmlwriter class
+     */
+    public static function begin_xml()
+    {
+        // Create the XML writer object
+        $xml = Writer::create_xml_writer();
+
+        // Write the XML header
+        Writer::write_xml_header($xml);
+
+        return $xml;
+    }
+
+
+    /**
+     * Write the XML representation of an argument to stdout
+     *  
+     * @param $xml - object of the xmlwriter class
+     * @param $num - number of the token in operation arguments
+     * @param $token - token to be written
+     */
+    public static function write_arg($xml, $num, $token)
+    {
+        // <var> or <const>
+        if (str_contains($token, "@")) {
+            $type = explode("@", $token)[0];
+
+            // <var>
+            if ($type == "GF" || $type == "TF" || $type == "LF") {
+                $type = "var";
+                $value = $token;
+            }
+            // <const>
+            else if ($type == "int" || $type == "bool" || $type == "string" || $type == "nil") {
+                $value = explode("@", $token)[1];
+            }
+        }
+        // <type> or <label>
+        else {
+            // <type>
+            if ($token == "int" || $token == "bool" || $token == "string" || $token == "nil") {
+                $type = "type";
+                $value = $token;
+            }
+            // <label>
+            else {
+                $type = "label";
+                $value = $token;
+            }
+        }
+
+        // Write the argument to stdout
+        xmlwriter_start_element($xml, "arg" . $num);
+        xmlwriter_start_attribute($xml, "type");
+        xmlwriter_text($xml, $type);
+        xmlwriter_end_attribute($xml);
+        xmlwriter_text($xml, $value);
+        xmlwriter_end_element($xml);
+    }
+
+    public static function finish_xml($xml)
+    {
+        // Finish the XML
+        xmlwriter_end_document($xml);
+
+        // Write XML to stdout
+        echo xmlwriter_output_memory($xml);
+    }
+}
 
 ?>
